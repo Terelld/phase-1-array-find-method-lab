400f0b1640c205e9dffc7d776055a5383310b077
Terell <Terelld@gmail.com>
2022-05-31 10:32:20 -0500

    commit tree afd5e05db62186604209ee3546b87a3bb3f674d7

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ] 
+
+//   array.find(function(currentValue, index, arr),thisValue);
+  
+// const superbowlWin = record.find((element) => {return element.name == 'W'});
+
+function superbowlWin(record) {
+    let wins = record.find (obj => obj.result === 'W')
+  if (wins) {
+      return wins.year
+  }
+} 
+
+
+
+// (wins ? record.year : undefined )
+// array.prototype.find(record => record = W)
+
+// function superbowlWin(result) {
+//     return (result === "W");
+// }
+
+// function superbowlWin(array) {
+//     const found = array.find(object => 
+//     // insert condition to match 
+//     //use dot notation to check the year property of object)
+
+//     return found
+// }
